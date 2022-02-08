0fc8271a15503aec3c795eb01f0f469f3a4b3075
alvissraghnall <isiomastewart@gmail.com>
2022-02-08 05:53:40 +0100

    skrrr

---
+++ src/main/java/com/alviss/TvShowTracker/TvShowTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.alviss.TvShowTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvShowTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TvShowTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alviss/TvShowTracker/TvShowTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alviss.TvShowTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TvShowTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
